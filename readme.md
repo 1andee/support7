d417139f95e5216d80c413995e682936e2e44c0f
Ian Duke <ian.c.duke@gmail.com>
2017-04-01 15:17:06 -0400

    initial commit
    initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+Support 7 call-out script to aid dispatchers based on present date and time.
---
+++ support7.js
@@ -0,0 +1,45 @@
+var hour = new Date().getHours();
+var day = new Date().getDay();
+var minutes = new Date().getMinutes();
+
+//Glenn	0300 to 1500 Mon to Thurs
+//	Fri-Sun 24 hrs
+
+
+if (day == 0 || day == 5 || day == 6) {
+   console.log("Call Glenn");
+}
+if (day >= 1 && day <= 4 && hour >= 2 && hour <= 14) {
+   console.log("Call Glenn");
+}
+
+
+//Louis	Availability:	Friday 1630 to Sunday 2000
+
+if (day == 5 && hour == 15 && minutes >= 30 || day == 5 && hour > 15 || day == 6 || day == 0 && hour < 19) {
+   console.log("Call Louis");
+}
+
+
+// Wayne Availability: Friday 1830 to Sunday 2100
+
+if (day == 5 && hour == 17 && minutes >= 30 || day == 5 && hour > 17 || day == 6 || day == 0 && hour < 20) {
+   console.log("Call Wayne");
+}
+
+
+// Everyone else that is available 24/7:
+
+   console.log("Call Jim (avail 24/7)");
+   console.log("Call Linda (avail 24/7)");
+   console.log("Call Desmond (avail 24/7)");
+   console.log("Call John C. (avail 24/7)");
+   console.log("Call Tony (avail 24/7)");
+   console.log("Call Paul (avail 24/7)");
+   console.log("Call Kevin (avail 24/7)");
+   console.log("Call Gord (avail 24/7)");
+   console.log("Call Jennifer (avail 24/7)");
+   console.log("Call Eric (avail 24/7)");
+   console.log("Call Michael (avail 24/7)");
+   console.log("Call Jeremy (avail 24/7)");
+   console.log("Call Will (avail 24/7)");
