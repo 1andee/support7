764ab50ff25de3bd3b19f73a7ec8ca0450ceb1e3
Ian Duke <ian.c.duke@gmail.com>
2017-04-02 20:53:39 -0400

    Fixed issue with timings

--- support7.js
+++ support7.js
@@ -2,28 +2,28 @@ var hour = new Date().getHours();
 var day = new Date().getDay();
 var minutes = new Date().getMinutes();
 
-//Glenn	0300 to 1500 Mon to Thurs
+//Glenn	Availability: 0300 to 1500 Mon to Thurs
 //	Fri-Sun 24 hrs
 
 
 if (day == 0 || day == 5 || day == 6) {
    console.log("Call Glenn");
 }
-if (day >= 1 && day <= 4 && hour >= 2 && hour <= 14) {
+if (day >= 1 && day <= 4 && hour >= 3 && hour <= 15) {
    console.log("Call Glenn");
 }
 
 
 //Louis	Availability:	Friday 1630 to Sunday 2000
 
-if (day == 5 && hour == 15 && minutes >= 30 || day == 5 && hour > 15 || day == 6 || day == 0 && hour < 19) {
+if (day == 5 && hour == 16 && minutes >= 30 || day == 5 && hour > 16 || day == 6 || day == 0 && hour < 20) {
    console.log("Call Louis");
 }
 
 
 // Wayne Availability: Friday 1830 to Sunday 2100
 
-if (day == 5 && hour == 17 && minutes >= 30 || day == 5 && hour > 17 || day == 6 || day == 0 && hour < 20) {
+if (day == 5 && hour == 18 && minutes >= 30 || day == 5 && hour > 18 || day == 6 || day == 0 && hour < 21) {
    console.log("Call Wayne");
 }
 
