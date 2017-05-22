de4a4f2726cc548e10d6487b22fdbff3834c7d0e
Ian Duke <ian.c.duke@gmail.com>
2017-05-22 01:12:33 -0400

    Built array and added for loop

--- support7.js
+++ support7.js
@@ -2,9 +2,9 @@ var hour = new Date().getHours();
 var day = new Date().getDay();
 var minutes = new Date().getMinutes();
 
+
 //Glenn	Availability: 0300 to 1500 Mon to Thurs
 //	Fri-Sun 24 hrs
-
 
 if (day == 0 || day == 5 || day == 6) {
    console.log("Call Glenn");
@@ -30,16 +30,8 @@ if (day == 5 && hour == 18 && minutes >= 30 || day == 5 && hour > 18 || day == 6
 
 // Everyone else that is available 24/7:
 
-   console.log("Call Jim (avail 24/7)");
-   console.log("Call Linda (avail 24/7)");
-   console.log("Call Desmond (avail 24/7)");
-   console.log("Call John C. (avail 24/7)");
-   console.log("Call Tony (avail 24/7)");
-   console.log("Call Paul (avail 24/7)");
-   console.log("Call Kevin (avail 24/7)");
-   console.log("Call Gord (avail 24/7)");
-   console.log("Call Jennifer (avail 24/7)");
-   console.log("Call Eric (avail 24/7)");
-   console.log("Call Michael (avail 24/7)");
-   console.log("Call Jeremy (avail 24/7)");
-   console.log("Call Will (avail 24/7)");
+var membersAlwaysAvail = ["Desmond","Eric","Gord","Jennifer","Jeremy","Jim","John C","Kevin","Linda","Michael","Paul","Tony","Will"];
+
+for (i = 0; i < membersAlwaysAvail.length; i++) {
+  console.log(membersAlwaysAvail[i]+" (avail 24/7)");
+}
